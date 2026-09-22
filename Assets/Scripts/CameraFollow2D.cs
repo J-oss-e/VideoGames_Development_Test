9c3aacf03ced62fa70d292167d4fcd4fa503f886
J-oss-e <josesilvaangelmunoz@gmail.com>
2026-09-22 01:52:45 -0600

    sidecroller/part_4.2
    Added smoothness to the camera movement.

--- Assets/Scripts/CameraFollow2D.cs
+++ Assets/Scripts/CameraFollow2D.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 public class CameraFollow2D : MonoBehaviour
 {
     [SerializeField] private Transform target;
+    [SerializeField] private float smoothTime = 0.2f;
+
+    private Vector3 velocity;
 
     // Update is called once per frame
     void LateUpdate()
     {
         if (target == null) return;
-        transform.position = new Vector3(target.position.x, target.position.y, -10f);
+
+        Vector3 desired = new Vector3(target.position.x, target.position.y, -10f);
+        transform.position = Vector3.SmoothDamp(transform.position, desired, ref velocity, smoothTime);
     }
 }
